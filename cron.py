88399c6a355c147c21cf752f3fc4d7883d62aca1
Montana McCallum <36941904+MontanaMcC@users.noreply.github.com>
2018-08-15 18:53:34 +1000

    Add files via upload

--- cron.py
+++ cron.py
@@ -7,9 +7,11 @@ cron.remove_all()
 
 #add write_to_db cron and temp_notify cron
 writejob = cron.new(command='/home/pi/Assignment_One/write_to_db.py')
-notifyjob = cron.new(command='home/pi/Assignment_One/temp_notify.py')
+notifyjob = cron.new(command='/home/pi/Assignment_One/temp_notify.py')
 
 #job settings
 writejob.minute.every(30)
+writejob.enable()
 notifyjob.minute.every(5)
+notifyjob.enable()
 cron.write()
--- write_to_db.py
+++ write_to_db.py
@@ -1,32 +1,34 @@
+#!/usr/bin/env python
 import sqlite3
+import os
 from sense_hat import SenseHat
 
 dbname='assignmentone.db'
 
 # get data from SenseHat sensor
 def getSenseHatData():	
-    sense = SenseHat()
+	sense = SenseHat()
 
-    temp = sense.get_temperature()
-    t = os.popen('/opt/vc/bin/vcgencmd measure_temp')
-    cputemp = t.read()
-    cputemp = cputemp.replace('temp=','')
-    cputemp = cputemp.replace('\'C\n','')
-    cputemp = float(cputemp)
-    newtemp = temp - ((cputemp - temp) / 2)
+	temp = sense.get_temperature()
+	t = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+	cputemp = t.read()
+	cputemp = cputemp.replace('temp=','')
+	cputemp = cputemp.replace('\'C\n','')
+	cputemp = float(cputemp)
+	newtemp = temp - ((cputemp - temp) / 2)
 
-    humidity = sense.get_humidity()
+	humidity = sense.get_humidity()
 
-    if newtemp is not None and humidity is not None:
-        temp = round(newtemp, 1)
-        humidity = round(humidity, 1)
-        logData (newtemp, humidity)
+	if newtemp is not None and humidity is not None:
+		newtemp = round(newtemp, 1)
+		humidity = round(humidity, 1)
+		logData (newtemp, humidity)
 
 # log sensor data on database
-def logData (temp, humidity):	
+def logData (newtemp, humidity):	
 	conn=sqlite3.connect(dbname)
 	curs=conn.cursor()
-	curs.execute("INSERT INTO ASSIGNMENTONE_data values(?, ?)", (temp, humidity))
+	curs.execute("INSERT INTO ASSIGNMENTONE_data values(?, ?)", (newtemp, humidity))
 	conn.commit()
 	conn.close()
 
