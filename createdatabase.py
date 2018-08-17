b8b84566f43c0b4833f5fd04d597f40536a53bb8
Montana McCallum <36941904+MontanaMcC@users.noreply.github.com>
2018-08-17 18:09:07 +1000

    Delete createdatabase.py

--- createdatabase.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as lite
-import sys
-con = lite.connect('assignmentone.db')
-with con: 
-    cur = con.cursor() 
-    cur.execute("DROP TABLE IF EXISTS ASSIGNMENTONE_data")
-    cur.execute("CREATE TABLE ASSIGNMENTONE_data(temp NUMERIC, humidity NUMERIC)")
